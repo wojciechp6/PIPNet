31621b6fb526a61bbb4a79beff7684862d235696
wpalka-icg <wpalka@icecodegames.com>
2025-04-16 10:11:49 +0200

    fixed tb

--- pipnet/test.py
+++ pipnet/test.py
@@ -115,6 +115,7 @@ def eval_pipnet(net,
 
     if tensorboard is not None:
         log_to_tensorboard(info, net.module, inputs, results, classes=test_loader.dataset.class_to_idx.values(), global_epoch=epoch, tb_writer=tensorboard)
+        tensorboard.flush()
 
     if net.module._num_classes == 2:
         tp = cm[0][0]
@@ -159,8 +160,12 @@ def log_to_tensorboard(info: dict, model_module, inp, preds, classes: List[str],
                 tb_writer.add_scalar(key, value, global_epoch)
 
     if hasattr(model_module, '_classification'):
-        weights = model_module._classification.weight.flatten()
-        tb_writer.add_figure('classification_weights', plot_tensor(weights, samples=66), global_epoch)
+        weights = model_module._classification.weight.flatten().cpu()
+        fig = plot_tensor(weights, samples=66)
+        tb_writer.add_figure('classification_weights', fig, global_epoch)
+
+        fig = plot_tensor(torch.relu(weights), samples=66)
+        tb_writer.add_figure('classification_non_neg', fig, global_epoch)
 
     inp = inp[0]
     tb_writer.add_graph(model_module, inp)
@@ -172,18 +177,40 @@ def log_to_tensorboard(info: dict, model_module, inp, preds, classes: List[str],
 def plot_batch(tensor: torch.Tensor, samples = 8, title: str = None):
     batch_size = tensor.shape[0]
     if batch_size > samples:
-        tensor = tensor[::batch_size//samples]
-    size = len(tensor)
-    plt.figure(figsize=(10, 10))
-    for i in range(len(tensor)):
-        plt.subplot(math.ceil(size / 2), 2, i+1)
-        plot_tensor(tensor[i])
-    return plt.gcf()
+        indices = torch.linspace(0, batch_size - 1, steps=samples).long()
+        sample_tensor = tensor[indices]
+    else:
+        sample_tensor = tensor
+
+    stats = [
+        ("Max over batch", torch.max(tensor, dim=0).values),
+        ("Min over batch", torch.min(tensor, dim=0).values),
+        ("Mean over batch", torch.mean(tensor, dim=0))
+    ]
+
+    fig = plt.figure(figsize=(12, 10))
+    if title:
+        fig.suptitle(title, fontsize=16)
+
+    ax = plt.subplot(2, 2, 1)
+    for s in sample_tensor:
+        plot_tensor(s, subplot=True)
+    ax.set_title("Samples")
+
+    for i, (stat_title, stat_tensor) in enumerate(stats, start=2):
+        ax = plt.subplot(2, 2, i)
+        plot_tensor(stat_tensor, subplot=True)
+        ax.set_title(stat_title)
+
+    plt.tight_layout()
+    return fig
 
 
-def plot_tensor(tensor: torch.Tensor, title: str = None, samples: int = 50):
+def plot_tensor(tensor: torch.Tensor, title: str = None, samples: int = 50, subplot: bool = False):
     array = tensor.cpu().numpy()
     array = smooth_array(array, samples)
+    if not subplot:
+        plt.figure()
     plt.plot(array)
     if title is not None:
         plt.title(title)
